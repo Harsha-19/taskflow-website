298e8674f08f4f8fdeadaf607e5044b2bc1ca737
Harsha <140263323+Harsha-19@users.noreply.github.com>
2026-03-19 23:28:11 +0530

    update backend

--- backend/run.py
+++ backend/run.py
@@ -1,20 +1,16 @@
 from __future__ import annotations
 
 from dotenv import load_dotenv
-from dotenv import load_dotenv
 
-import os
+
 from app import create_app
 import os
 
 def main() -> None:
     load_dotenv()
     app = create_app()
-    port = int(os.environ.get("PORT", 5000))
+    port=int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port) #debug=True
 
 if __name__ == "__main__":
-    app.run(
-        host="0.0.0.0",
-        port=int(os.environ.get("PORT", 10000))
-    )
+    main()
